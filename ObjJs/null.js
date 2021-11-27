c6e08e62037a1d224f2e4499717a6a4f0c759b19
Savecoders <pablopincay2003@gmail.com>
2021-11-27 18:08:18 -0500

    add null.js

--- ObjJs/null.js
+++ ObjJs/null.js
@@ -1,15 +1,9 @@
-function suma(a,b) {
- if(!a||!b){
-    console.log("Debes intriducir 2  valores")
-    return;
-  }
- return a+b; 
-}
-const result = suma(2,2) ? suma(2,2):"no hay resultado"
-//console.log(result)
-function newSuma(a=0,b=0) {
-  return a+b
+const suma = (a, b) => (a & b ? a + b : 'no hay resultado, debes introducir 2 numeros');
+const result = suma(2, 2);
+console.log(result);
+function newSuma(a = 0, b = 0) {
+	return a + b;
 }
 //console.log(newSuma(2,2))
-const sumaArrow = (a=0,b=0)=>a+b
-console.log(sumaArrow(3))
+const sumaArrow = (a = 0, b = 0) => a + b;
+//console.log(sumaArrow(3))
